57f54c2b6a4b1c06776a80f9bbb340340506ab51
sraman26 <52307717+sraman26@users.noreply.github.com>
2019-07-04 18:29:51 +0530

    Add files via upload

---
+++ stilvol/js/js-image-slider.js
@@ -0,0 +1,16 @@
+var sliderOptions =
+{
+    sliderId: "slider",
+    startSlide: 0,
+    effect: "series1",
+    effectRandom: false,
+    pauseTime: 2800,
+    transitionTime: 1200,
+    slices: 14,
+    boxes: 8,
+    hoverPause: 1,
+    autoAdvance: true,
+    thumbnailsWrapperId: "thumb_container_id",
+    m: false,
+    license: "mylicense"
+};// JavaScript Document
